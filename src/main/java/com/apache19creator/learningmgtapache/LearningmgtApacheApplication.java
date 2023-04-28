3748f150f4aac617eaa92ceeb4f99094a8266f7a
Wale Orire <saheedadewale90@gmail.com>
2023-04-28 16:32:04 +0100

    setup

---
+++ src/main/java/com/apache19creator/learningmgtapache/LearningmgtApacheApplication.java
@@ -0,0 +1,13 @@
+package com.apache19creator.learningmgtapache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmgtApacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningmgtApacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apache19creator/learningmgtapache/LearningmgtApacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache19creator.learningmgtapache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmgtApacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
